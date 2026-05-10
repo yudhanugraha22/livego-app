bdd70064ef4aa214766c6ea7e025a7853bba27ea
yudhanugraha22 <examplepart1@gmail.com>
2026-05-10 19:59:17 +0700

    chore: wipe out broken legacy files and start fresh build

--- lib/routes/app_routes.dart
+++
@@ -1,8 +0,0 @@
-// lib/routes/app_routes.dart
-
-class AppRoutes {
-  static const String initial = '/';
-  static const String detail = '/detail';
-  static const String player = '/player';
-  static const String settings = '/settings';
-}
