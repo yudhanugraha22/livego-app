6e83dee8e78b11f91d4f487be67b01ae3bd5934a
yudhanugraha22 <examplepart1@gmail.com>
2026-05-10 12:23:54 +0700

    Build 110: Fixed Directory Creation

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'ui/mobile/mobile_home.dart';
 import 'ui/tv/tv_home.dart';
-
 void main() => runApp(const ProviderScope(child: LivegoApp()));
-
 class LivegoApp extends StatelessWidget {
   const LivegoApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: const MainSwitcher(),
-    );
+  @override Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, theme: ThemeData.dark(), home: const MainSwitcher());
   }
 }
-
 class MainSwitcher extends StatelessWidget {
   const MainSwitcher({super.key});
-  @override
-  Widget build(BuildContext context) {
-    bool isTV = MediaQuery.of(context).size.width > 900;
-    return isTV ? const TVHome() : const MobileHome();
+  @override Widget build(BuildContext context) {
+    return MediaQuery.of(context).size.width > 900 ? const TVHome() : const MobileHome();
   }
 }
--- lib/ui/mobile/mobile_home.dart
+++ lib/ui/mobile/mobile_home.dart
@@ -1,25 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../providers/app_providers.dart';
-
-class MobileHome extends ConsumerWidget {
+class MobileHome extends StatelessWidget {
   const MobileHome({super.key});
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final dramas = ref.watch(homeDataProvider);
-    return Scaffold(
-      backgroundColor: const Color(0xFF0D1117),
-      appBar: AppBar(title: const Text("Livego Mobile"), backgroundColor: const Color(0xFF161B22)),
-      body: dramas.when(
-        data: (list) => GridView.builder(
-          padding: const EdgeInsets.all(10),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4, childAspectRatio: 0.65, mainAxisSpacing: 8, crossAxisSpacing: 8),
-          itemCount: list.length,
-          itemBuilder: (c, i) => ClipRRect(borderRadius: BorderRadius.circular(8), child: Image.network(list[i]['cover'], fit: BoxFit.cover)),
-        ),
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (e, s) => const Center(child: Text("Gagal memuat data")),
-      ),
-    );
-  }
+  @override Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: Text("Livego Mobile")), body: Center(child: Text("Home Mobile Ready")));
 }
