cb52a647007db64a0dbbcfd369b158dc10fcc21c
Celosz-fle7 <ashtertimur@gmail.com>
2026-05-11 20:23:21 +0700

    Build 126: Dynamic Banner Slider with Platform Sync

--- lib/providers/app_providers.dart
+++ lib/providers/app_providers.dart
@@ -4,18 +4,20 @@ import '../core/api_engine.dart';
 final platformProvider = StateProvider<String>((ref) => "melolo");
 final categoryProvider = StateProvider<String>((ref) => "Dubbing");
 
+// Provider untuk mengambil LIST BANNER (Bisa digeser)
+final bannerProvider = FutureProvider<List>((ref) async {
+  final plat = ref.watch(platformProvider);
+  final res = await ApiEngine.request("/api/v2/banner?category_p=$plat&lang=id");
+  if (res != null && res['success'] == true) {
+    return res['data'];
+  }
+  return [];
+});
+
 final dramasProvider = FutureProvider<List>((ref) async {
   final plat = ref.watch(platformProvider);
   final cat = ref.watch(categoryProvider);
-  String p = (cat == "Dubbing") 
-    ? "/api/v2/search?category_p=$plat&q=sulih suara&lang=id" 
-    : "/api/v2/home?category_p=$plat&lang=id";
+  String p = (cat == "Dubbing") ? "/api/v2/search?category_p=$plat&q=sulih suara&lang=id" : "/api/v2/home?category_p=$plat&lang=id";
   final res = await ApiEngine.request(p);
   return res != null ? res['data'] : [];
-});
-
-final bannerProvider = FutureProvider<Map?>((ref) async {
-  final plat = ref.watch(platformProvider);
-  final res = await ApiEngine.request("/api/v2/banner?category_p=$plat&lang=id");
-  return (res != null && res['data'].isNotEmpty) ? res['data'][0] : null;
 });
